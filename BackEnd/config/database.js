e88788cdfaf8172a224d7a025cb6a7401da1fef3
bhumika3047 <bhumiprajapati216@gmail.com>
2025-05-30 18:58:57 +0530

    Remove comments

--- BackEnd/config/database.js
+++ BackEnd/config/database.js
@@ -3,14 +3,13 @@ const mongoose = require("mongoose");
 const { MONGO_URI } = process.env;
 
 exports.connect = () => {
-  // Connecting to the database
   mongoose
     .connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then(() => {
-      console.log("Successfully connected to database");
+      console.log("Successfully connected to bhumika quiz database");
     })
     .catch((error) => {
       console.log("database connection failed. exiting now...");
--- BackEnd/model/user.js
+++ BackEnd/model/user.js
@@ -4,7 +4,6 @@ const userSchema = new mongoose.Schema({
   user: { type: String, default: null },
   email: { type: String, unique: true },
   password: { type: String },
-  // token: { type: String },
 });
 
 module.exports = mongoose.model("user", userSchema);
